112d320b4fd5beb710dd42f38f1567bd723cfdd2
CEDipEngineering <cadudip@hotmail.com>
2022-05-23 08:40:43 -0300

    Added canvas fingerprinting and connection listing to cookie counter.

--- cookieGrabber.js
+++ cookieGrabber.js
@@ -1,18 +1,27 @@
+// https://stackoverflow.com/questions/14226803/wait-5-seconds-before-executing-next-line
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const getConnections = async () => {
+    await delay(100);
+    var countReqs = window.performance.getEntriesByType("resource").length;
+    alert(countReqs.toString() + " requests made by this page!")
+}
 
 
-alert("Grabbing cookies");
+function grabCookies(){
+    let cookies = document.cookie;
+    const cArr = decodeURIComponent(cookies).split(';');
+    alert("Found " + cArr.length.toString() + " cookies!");
+}
 
-console.log("Grabbing cookies...");
-
-let cookies = document.cookie;
-const cArr = decodeURIComponent(cookies).split(';');
-alert("Found " + cArr.length.toString() + " cookies!");
-
-// cArr.forEach(x => {alert(x);});
-// let localCookies, externalCookies;
-
-// cArr.forEach(x => {
-    
+function isCanvasFingerprinting(){
+    let arr = document.querySelectorAll("canvas");
+    if (arr.length > 0){
+        alert("Possible canvas fingerprinting!");
+    }
+}
 
 
-// });
+getConnections();
+grabCookies();
+isCanvasFingerprinting();
