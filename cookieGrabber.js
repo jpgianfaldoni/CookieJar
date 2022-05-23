cb320cb0bda8e675a500dbe9001ee3f04e4fa653
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2022-05-23 13:34:30 -0300

    add more functions to cookieGrabber.js

--- cookieGrabber.js
+++ cookieGrabber.js
@@ -22,6 +22,37 @@ function isCanvasFingerprinting(){
 }
 
 
+function showLocalStorage(){
+    var _lsTotal = 0,
+    _xLen, _x;
+    for (_x in localStorage) {
+        if (!localStorage.hasOwnProperty(_x)) {
+            continue;
+        }
+        _xLen = ((localStorage[_x].length + _x.length) * 2);
+        _lsTotal += _xLen;
+    };
+    alert("Local Storage = " + (_lsTotal / 1024).toFixed(2) + " KB");
+}
+
+function getExternalConnections(){
+    var originalUrl = window.location.origin
+    var cleanUrl = originalUrl.replace(/^https?:\/\//, '').replace('www\.', '')
+    var resourceList = window.performance.getEntries();
+    var nExternalConnections = 0 
+
+    for (i = 0; i < resourceList.length; i++)
+        {
+            if(!resourceList[i].name.includes(cleanUrl)){
+                nExternalConnections ++
+            }
+        }
+    alert("N of external connections: " + nExternalConnections)
+}
+
+
 getConnections();
 grabCookies();
 isCanvasFingerprinting();
+showLocalStorage();
+getExternalConnections();
