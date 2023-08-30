359c5d3637248293106626dc7dc11deca8caf290
jpgianfaldoni <jpgianfaldoni@gmail.com>
2023-08-30 08:00:19 -0300

    add documentation

--- src/CookieJar.js
+++ src/CookieJar.js
@@ -1,31 +1,26 @@
-
 // Shows local storage size
 var _lsTotal = 0,
-    _xLen, _x;
+  _xLen,
+  _x;
 for (_x in localStorage) {
-    if (!localStorage.hasOwnProperty(_x)) {
-        continue;
-    }
-    _xLen = ((localStorage[_x].length + _x.length) * 2);
-    _lsTotal += _xLen;
-    alert(localStorage[_x]);
-};
+  if (!localStorage.hasOwnProperty(_x)) {
+    continue;
+  }
+  _xLen = (localStorage[_x].length + _x.length) * 2;
+  _lsTotal += _xLen;
+  alert(localStorage[_x]);
+}
 alert("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
 
-
-
-
-
 // Shows number of 3rd part domains connections
-var originalUrl = window.location.origin
-var cleanUrl = originalUrl.replace(/^https?:\/\//, '').replace('www\.', '')
+var originalUrl = window.location.origin;
+var cleanUrl = originalUrl.replace(/^https?:\/\//, "").replace("www.", "");
 var resourceList = window.performance.getEntries();
-var nExternalConnections = 0 
+var nExternalConnections = 0;
 
-for (i = 0; i < resourceList.length; i++)
-    {
-        if(!resourceList[i].name.includes(cleanUrl)){
-            nExternalConnections ++
-        }
-    }
-alert(nExternalConnections)
+for (i = 0; i < resourceList.length; i++) {
+  if (!resourceList[i].name.includes(cleanUrl)) {
+    nExternalConnections++;
+  }
+}
+alert(nExternalConnections);
